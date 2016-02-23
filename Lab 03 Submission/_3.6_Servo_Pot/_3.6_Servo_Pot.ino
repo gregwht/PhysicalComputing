34557a57ee11be6adae036c94382c7ac2fb50cf4
gregcw <gregcwbusiness@gmail.com>
2016-02-23 19:34:20 +0000

    Adding code

---
+++ Lab 03 Submission/_3.6_Servo_Pot/_3.6_Servo_Pot.ino
@@ -0,0 +1,29 @@
+/* 
+ LED Dimmer code from http://www.toptechboy.com/arduino/lesson-11-arduino-circuit-to-dim-led-with-potentiometer/
+ 
+ Servo code from Scott Fitzgerald http://arduino.cc/en/Tutorial/Knob
+*/
+
+#include <Servo.h> 
+ 
+Servo myservo;  // create servo object to control a servo 
+ 
+int potpin = 0;  // analog pin used to connect the potentiometer
+int readValue;   // Reading pot
+int serValue;     // Writing to servo
+
+ 
+void setup() 
+{ 
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+  Serial.begin(9600);
+} 
+ 
+void loop() 
+{ 
+  readValue = analogRead(potpin);              // reads the value of the potentiometer (value between 0 and 1023) 
+  serValue = map(readValue, 0, 1023, 0, 180);  // scale it to use it with the servo (value between 0 and 180) 
+  myservo.write(serValue);                     // sets the servo position according to the scaled value 
+  delay(15);                                   // waits for the servo to get there 
+
+} 
